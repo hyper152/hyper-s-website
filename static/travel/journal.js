675c777e050cff984ee1f5509bf2fa694a801578
hyper152 <2361542526@qq.com>
2026-09-09 13:05:36 +0800

    update

---
+++ static/travel/journal.js
@@ -0,0 +1,4 @@
+const viewer=document.querySelector('#viewer');
+document.querySelectorAll('.photo').forEach(button=>button.addEventListener('click',()=>{const image=button.querySelector('img');viewer.querySelector('img').src=image.src;viewer.querySelector('img').alt=image.alt;viewer.querySelector('p').textContent=image.alt;viewer.querySelector('a').href=button.dataset.original;viewer.showModal();}));
+viewer.querySelector('button').addEventListener('click',()=>viewer.close());
+viewer.addEventListener('click',event=>{if(event.target===viewer){const r=viewer.getBoundingClientRect();if(event.clientX<r.left||event.clientX>r.right||event.clientY<r.top||event.clientY>r.bottom)viewer.close();}});
